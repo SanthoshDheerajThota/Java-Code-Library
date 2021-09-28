51f96e72c6406ab80970dc65ec77b3c5ea20727a
Thota Santhosh Dheeraj <85696638+SanthoshDheerajThota@users.noreply.github.com>
2021-09-28 22:42:55 +0530

    Create JavaCode.java

---
+++ JavaCode.java
@@ -0,0 +1,12 @@
+import professions.Programmer;
+import professions.Professions;
+class deleteNow{
+    public static void main(String[] args) {
+        Professions p1=new Professions();
+        p1.getData();
+        p1.PrintData();
+        Programmer santhosh=new Programmer();
+        santhosh.getData();
+        santhosh.printData();
+    }
+}
